67f0a7889ad3729da14298ebd689cca6d61ebba0
Robson Junior <jrobsonjr16@gmail.com>
2017-03-12 19:05:53 -0300

    Início da implementação de ParticipacaoService.

--- src/service/ParticipacaoService.java
+++ src/service/ParticipacaoService.java
@@ -1,23 +1,21 @@
 package service;
 
+import participacao.AlunoGraduando;
+import participacao.Professor;
+import participacao.Profissional;
+
 public class ParticipacaoService {
 
-	public void associaProfessor(String cpfPessoa, String codigoProjeto, boolean coordenador, double valorHora,
-			int qntHoras) {
-		// TODO Auto-generated method stub
-		
+	public Professor associaProfessor(boolean coordenador, double valorHora, int qntHoras) {
+		return new Professor(valorHora, qntHoras, coordenador);
 	}
 
-	public void associaGraduando(String cpfPessoa, String codigoProjeto, double valorHora, int qntHoras) {
-		// TODO Auto-generated method stub
-		
+	public AlunoGraduando associaGraduando(double valorHora, int qntHoras) {
+		return new AlunoGraduando(valorHora, qntHoras);
 	}
 
-	public void associaProfissional(String cpfPessoa, String codigoProjeto, String cargo, double valorHora,
-			int qntHoras) {
-		// TODO Auto-generated method stub
-		
+	public Profissional associaProfissional(String cargo, double valorHora, int qntHoras) {
+		return new Profissional(valorHora, qntHoras, cargo);
 	}
-
 
 }

6479412894f6243a1bb6ccc43b8831cfb1a99b74
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2017-03-15 15:07:09 -0300

    validacoes concluidas

--- src/validacao/validaPessoa.java
+++ src/validacao/validaPessoa.java
@@ -2,21 +2,39 @@ package validacao;
 
 import exception.StringInvalidaException;
 
-public class validaPessoa {
+public class ValidaPessoa {
 
 	public static void validaNome(String nome) throws StringInvalidaException {
-		if (nome == null) {
-			throw new StringInvalidaException("Nome nulo ou vazio");
-		}
-		if (nome.trim().equals("")){
-			throw new StringInvalidaException("Nome nulo ou vazio");			
+		try {
+			ModuloDeValidacao.stringInvalida(nome);			
+		} catch (StringInvalidaException e) {
+			throw new StringInvalidaException("Nome " + e.getMessage());
 		}
 	}
 
 	public static void validaEmail(String email) throws Exception {
-		String formaEmail = "[a-zA-Z0-9]@[a-zA-Z0-9].com";
-		if (!(email.matches("[a-zA-Z0-9]@[a-zA-Z0-9].com"))) {
+		try {
+			ModuloDeValidacao.stringInvalida(email);			
+		} catch (StringInvalidaException e) {
+			throw new StringInvalidaException("Email " + e.getMessage());
+		}
+		
+		String formaEmail = "[\\._a-zA-Z0-9]+@[a-zA-Z0-9]+(\\.[a-zA-z]+)+";
+		if (!email.matches(formaEmail)) {
 			throw new Exception("Email invalido");
+		}
+	}
+
+	public static void validaCpf(String cpf) throws StringInvalidaException {
+		try {
+			ModuloDeValidacao.stringInvalida(cpf);			
+		} catch (StringInvalidaException e) {
+			throw new StringInvalidaException("CPF " + e.getMessage());
+		}		
+		
+		String formaCpf = "\\d{3}.\\d{3}.\\d{3}-\\d{2}";
+		if (!cpf.matches(formaCpf)){
+			throw new StringInvalidaException("CPF invalido");
 		}
 	}
 

36ced342606359cab65a13060e0c617d7c4f8eb7
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2017-03-14 21:33:25 -0300

    implementados metodos de ProjetoController

--- src/projeto/ProjetoController.java
+++ src/projeto/ProjetoController.java
@@ -1,11 +1,113 @@
 package projeto;
 
 import projeto.ProjetoService;
+import validacao.ModuloDeValidacao;
 
 public class ProjetoController {
-	private ProjetoService service;
+	private ProjetoService projetoService;
 	
 	public ProjetoController(){
-		service = new ProjetoService();
+		projetoService = new ProjetoService();
 	}
+	
+	
+	public String adicionaMonitoria(String nome, String disciplina, int rendimento, String objetivo, String periodo,
+			String dataInicio, int duracao) throws Exception {
+		try {
+			ModuloDeValidacao.stringInvalida(nome);
+			ModuloDeValidacao.stringInvalida(disciplina);
+			ModuloDeValidacao.rendimentoInvalido(rendimento);
+			ModuloDeValidacao.stringInvalida(objetivo);
+			ModuloDeValidacao.stringInvalida(periodo);
+			ModuloDeValidacao.stringInvalida(dataInicio);
+			ModuloDeValidacao.duracaoInvalida(duracao);
+		} catch (Exception e) {
+			throw new Exception("Erro no cadastro de projeto: " + e.getMessage());
+		}
+
+		return projetoService.adicionaMonitoria(nome, disciplina, rendimento, objetivo, periodo, dataInicio, duracao);
+	}
+
+	public String adicionaPET(String nome, String objetivo, int impacto, int rendimento, int prodTecnica,
+			int prodAcademica, int patentes, String dataInicio, int duracao) throws Exception {
+		try {
+			ModuloDeValidacao.stringInvalida(nome);
+			ModuloDeValidacao.stringInvalida(objetivo);
+			ModuloDeValidacao.impactoInvalido(impacto);
+			ModuloDeValidacao.rendimentoInvalido(rendimento);
+			ModuloDeValidacao.inteiroInvalido(prodTecnica);
+			ModuloDeValidacao.inteiroInvalido(prodAcademica);
+			ModuloDeValidacao.inteiroInvalido(patentes);
+			ModuloDeValidacao.dataInvalida(dataInicio);
+			ModuloDeValidacao.duracaoInvalida(duracao);
+		} catch (Exception e) {
+			throw new Exception("Erro no cadastro de projeto: " + e.getMessage());
+		}
+
+		return projetoService.adicionaPET(nome, objetivo, impacto, rendimento, prodTecnica, prodAcademica, patentes,
+				dataInicio, duracao);
+	}
+
+	public String adicionaExtensao(String nome, String objetivo, int impacto, String dataInicio, int duracao)
+			throws Exception {
+		try {
+			ModuloDeValidacao.stringInvalida(nome);
+			ModuloDeValidacao.stringInvalida(objetivo);
+			ModuloDeValidacao.impactoInvalido(impacto);
+			ModuloDeValidacao.dataInvalida(dataInicio);
+			ModuloDeValidacao.duracaoInvalida(duracao);
+		} catch (Exception e) {
+			throw new Exception("Erro no cadastro de projeto: " + e.getMessage());
+		}
+
+		return projetoService.adicionaExtensao(nome, objetivo, impacto, dataInicio, duracao);
+	}
+
+	public String adicionaPED(String nome, String categoria, int prodTecnica, int prodAcademica, int patentes,
+			String objetivo, String dataInicio, int duracao) throws Exception {
+		try {
+			ModuloDeValidacao.stringInvalida(nome);
+			ModuloDeValidacao.stringInvalida(categoria);
+			ModuloDeValidacao.inteiroInvalido(prodTecnica);
+			ModuloDeValidacao.inteiroInvalido(prodAcademica);
+			ModuloDeValidacao.inteiroInvalido(patentes);
+			ModuloDeValidacao.stringInvalida(objetivo);
+			ModuloDeValidacao.dataInvalida(dataInicio);
+			ModuloDeValidacao.duracaoInvalida(duracao);
+		} catch (Exception e) {
+			throw new Exception("Erro no cadastro de projeto: " + e.getMessage());
+		}
+
+		return projetoService.adicionaPED(nome, categoria, prodTecnica, prodAcademica, patentes, objetivo, dataInicio,
+				duracao);
+	}
+
+	public void editaProjeto(String codigo, String atributo, String valor) throws Exception {
+		try {
+			if (atributo.equalsIgnoreCase("Objetivo")) {
+				ModuloDeValidacao.objetivoInvalido(valor);
+			} else {
+				ModuloDeValidacao.dataInvalida(valor);
+			}
+		} catch (Exception e) {
+			throw new Exception("Erro na atualizacao de projeto: " + e.getMessage());
+		}
+		projetoService.editaProjeto(codigo, atributo, valor);
+	}
+
+	public void removeProjeto(String codigo) {
+		projetoService.removeProjeto(codigo);
+	}
+
+	// TODO AJEITAR OS TESTES
+	public String getInfoProjeto(String codigo, String atributo) throws Exception {
+		ModuloDeValidacao.codigoInvalido(codigo);
+		atributo.toLowerCase();
+		if (!(atributo.equals("data de inicio") || atributo.equals("objetivo"))) {
+			throw new Exception("Atributo invalido.");
+		}
+
+		return projetoService.getInfoProjeto(codigo, atributo);
+	}
+	
 }
